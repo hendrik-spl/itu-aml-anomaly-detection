0bb640543752c434304d8aadcce8044c47b5c0c5
hendrik-spl <145655111+hendrik-spl@users.noreply.github.com>
2024-11-24 21:48:47 +0100

    refactor plot_history function to simplify accuracy plotting and enhance loss reporting

--- evaluation/plots.py
+++ evaluation/plots.py
@@ -15,13 +15,6 @@ def plot_history(comment, history):
     plt.figure(figsize=(14, 4))
     plt.suptitle(comment)
 
-    plt.subplot(1, 2, 1)
-    plt.plot(history.history['accuracy'], label='Training Accuracy')
-    plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
-    plt.xlabel('Epoch')
-    plt.ylabel('Accuracy')
-    plt.legend()
-
     plt.subplot(1, 2, 2)
     plt.plot(history.history['loss'], label='Training Loss')
     plt.plot(history.history['val_loss'], label='Validation Loss')
@@ -31,8 +24,6 @@ def plot_history(comment, history):
 
     plt.show()
 
-    print(f'Best train_accuracy: {np.max(history.history["accuracy"]).round(4)}')
     print(f'Best train_loss: {np.min(history.history["loss"]).round(4)}')
-    print(f'Best val_accuracy: {np.max(history.history["val_accuracy"]).round(4)}')
     print(f'Best val_loss: {np.min(history.history["val_loss"]).round(4)}')
-    print(f'Last improvement at epoch: {np.argmax(history.history["val_accuracy"])+1}')
+    print(f'Last improvement of val_loss at epoch: {np.argmax(history.history["val_loss"])+1}')

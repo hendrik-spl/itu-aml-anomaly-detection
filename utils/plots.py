correct calculation of last improvement epoch for validation loss

utils/plots.py
@@ -27,7 +27,7 @@ def plot_history(comment, history):
 
     print(f'Best train_loss: {np.min(history.history["loss"]).round(4)}')
     print(f'Best val_loss: {np.min(history.history["val_loss"]).round(4)}')
-    print(f'Last improvement of val_loss at epoch: {np.argmax(history.history["val_loss"])+1}')
+    print(f'Last improvement of val_loss at epoch: {np.argmin(history.history["val_loss"])+1}')
 
 def plot_reconstructions(autoencoder: Model, test_generator: ImageDataGenerator, n_images: int, title) -> None:
     """